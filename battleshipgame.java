bd2f9c4e9cd846071a7a876c4d9f0f3c46aa51c8
FungusSir <fergus.chui@gmail.com>
2022-05-30 10:39:57 -0400

    added menu bar

--- battleshipgame.java
+++ battleshipgame.java
@@ -2,12 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import java.io.*;
 
 public class BattleshipGame implements ActionListener{
 	//Properties
 	JFrame theFrame = new JFrame("Battleship");
 	BattleshipPanel thePanel = new BattleshipPanel();
 	Timer theTimer = new Timer(1000/60, this);
+	
+	JMenuBar theBar = new JMenuBar();
+	JMenu theHome = new JMenu("Home");
+	JMenu theHelp = new JMenu("Help");
+	JMenu theQuit = new JMenu("Quit");
 	
 	//Methods
 	public void actionPerformed(ActionEvent evt){
@@ -22,9 +28,15 @@ public class BattleshipGame implements ActionListener{
 		theFrame.setContentPane(thePanel);
 		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		theFrame.pack();
+		theFrame.setJMenuBar(theBar);
 		theFrame.setVisible(true);	
 		theFrame.setResizable(false);
 		theTimer.start();
+		
+		//Add JItems
+		theBar.add(theHome);
+		theBar.add(theHelp);
+		theBar.add(theQuit);
 	}
 
 	//Main Method
