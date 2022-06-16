b772fdd77e2e7fe7ca09f90b911b1082abd1cbd7
Louis Sun <louis.sun3570@gmail.com>
2022-06-15 23:33:29 -0400

    Completed help
    - Added all help screens
    - Made it interactive

--- BattleshipGame.java
+++ BattleshipGame.java
@@ -38,7 +38,28 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 	BattleshipHelpPanel helpPanel = new BattleshipHelpPanel();
 	BattleshipHelpPanel2 helpPanel2 = new BattleshipHelpPanel2();
 	JButton nextButton1 = new JButton("Next");
+	JButton nextButton2 = new JButton("Next");
+	JButton nextButton3 = new JButton("Next");
+	JButton nextButton4 = new JButton("Next");
+	JButton nextButton5 = new JButton("Next");
+	JButton nextButton6 = new JButton("Next");
+	JButton nextButton7 = new JButton("Next");
 	JTextField step1 = new JTextField("Demo Step 1: Drag and Drop your ships onto your map");
+	JTextField step1rotate = new JTextField("Click the rotate button BEFORE PLACING YOUR SHIPS to rotate them");
+	JTextField step1ready = new JTextField("Click the ready button when you've placed all 5 ships");
+	JButton step1rotateButton = new JButton("Rotate");
+	JButton step1readyButton = new JButton("Ready");
+	JTextField step1yourMsgField = new JTextField();
+	JTextField step1opponentMsgField = new JTextField();
+	JTextField yourmsgfield = new JTextField("This top chat area displays YOUR messages");
+	JTextField opponentmsgfield = new JTextField("This bottom chat area displays OPPONENT messages");
+	JTextField bothready = new JTextField("Once both players are ready, your ships get moved to the minimap");
+	JTextField step2 = new JTextField("Demo Step 2: Guess where your opponent's ships are located by clicking on a blue square/coordinate");
+	JTextField step2info = new JTextField("You take turns guessing"); 
+	JTextField step2hit = new JTextField("Try clicking on a square to see what a hit looks like! If you hit, you go again!");
+	JTextField step2hitinfo = new JTextField("Once a ship has been sunk, it will show up on the screen (not in demo)");
+	JTextField step2miss = new JTextField("Try clicking on a square to see what a miss looks like!");
+	JTextField step2missinfo = new JTextField("Notice that after you have guessed a square, you can't click it again");
 	
 	//Themes Panel
 	BattleshipThemesPanel themesPanel = new BattleshipThemesPanel();
@@ -55,28 +76,49 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 				helpPanel2.repaint();
 			}
 		} else if (evt.getSource() == animTimer) {
-			for (int intAnimation=1; intAnimation<=3; intAnimation++) {
-				if (playPanel.blnPlayAnimation[intAnimation] == true) {
-					playPanel.repaint();
-					playPanel.intAnimCount++;
+			if (theFrame.getContentPane() == playPanel) {
+				for (int intAnimation=1; intAnimation<=3; intAnimation++) {
+					if (playPanel.blnPlayAnimation[intAnimation] == true) {
+						playPanel.repaint();
+						playPanel.intAnimCount++;
+							
+						if(playPanel.intAnimCount > playPanel.intMaxAnimationSprites[intAnimation]){
+							playPanel.blnPlayAnimation[intAnimation] = false;
+							playPanel.intAnimCount = 0;
+						}
 						
-					if(playPanel.intAnimCount > playPanel.intMaxAnimationSprites[intAnimation]){
-						playPanel.blnPlayAnimation[intAnimation] = false;
-						playPanel.intAnimCount = 0;
+						break; // So it doesn't play 2 animations at the same time
 					}
+				}
+				
+				if (playPanel.blnPlayAnimation[1] == false && playPanel.blnPlayAnimation[2] == false && playPanel.blnPlayAnimation[3] == false) {
+					playPanel.blnPlayingAnimation = false;
 					
-					break; // So it doesn't play 2 animations at the same time
+					if (playPanel.intSunkValue != 0) {
+						playPanel.intOpponentGrid[playPanel.intSunkRow][playPanel.intSunkCol] = playPanel.intSunkValue;
+						playPanel.intSunkRow = playPanel.intSunkCol = playPanel.intSunkValue = 0;
+					}
+				}
+			} else if (theFrame.getContentPane() == helpPanel2) {
+				for (int intAnimation=1; intAnimation<=3; intAnimation++) {
+					if (helpPanel2.blnPlayAnimation[intAnimation] == true) {
+						helpPanel2.repaint();
+						helpPanel2.intAnimCount++;
+							
+						if(helpPanel2.intAnimCount > helpPanel2.intMaxAnimationSprites[intAnimation]){
+							helpPanel2.blnPlayAnimation[intAnimation] = false;
+							helpPanel2.intAnimCount = 0;
+						}
+						
+						break; // So it doesn't play 2 animations at the same time
+					}
+				}
+				
+				if (helpPanel2.blnPlayAnimation[1] == false && helpPanel2.blnPlayAnimation[2] == false && helpPanel2.blnPlayAnimation[3] == false) {
+					helpPanel2.blnPlayingAnimation = false;
 				}
 			}
 			
-			if (playPanel.blnPlayAnimation[1] == false && playPanel.blnPlayAnimation[2] == false && playPanel.blnPlayAnimation[3] == false) {
-				playPanel.blnPlayingAnimation = false;
-				
-				if (playPanel.intSunkValue != 0) {
-					playPanel.intOpponentGrid[playPanel.intSunkRow][playPanel.intSunkCol] = playPanel.intSunkValue;
-					playPanel.intSunkRow = playPanel.intSunkCol = playPanel.intSunkValue = 0;
-				}
-			}
 		}
 		else if (evt.getSource() == hostButton) {
 			ssm = new SuperSocketMaster(9001, this);
@@ -230,9 +272,81 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 			theFrame.setContentPane(helpPanel2);
 			theFrame.pack();
 			helpPanel2.repaint();
+		} else if (evt.getSource() == step1rotateButton) {
+			if (helpPanel2.intPositionX == 800 && helpPanel2.intPositionY == 100) {
+				helpPanel2.blnHorizontal = !helpPanel2.blnHorizontal;
+			}
+		} else if (evt.getSource() == step1readyButton) {
+			if (helpPanel2.intPositionX != 800 && helpPanel2.intPositionY != 100) {
+				step1yourMsgField.setText("READY");
+			}
+		} else if (evt.getSource() == nextButton2) {
+			step1.setVisible(false);
+			step1rotate.setVisible(false);
+			step1ready.setVisible(false);
+			nextButton2.setVisible(false);
+			yourmsgfield.setVisible(true);
+			opponentmsgfield.setVisible(true);
+			nextButton3.setVisible(true);
+		} else if (evt.getSource() == nextButton3) {
+			yourmsgfield.setVisible(false);
+			opponentmsgfield.setVisible(false);
+			nextButton3.setVisible(false);
+			bothready.setVisible(true);
+			step1opponentMsgField.setText("READY");
+			helpPanel2.blnReady = true;
+			nextButton4.setVisible(true);
+		} else if (evt.getSource() == nextButton4) {
+			bothready.setVisible(false);
+			nextButton4.setVisible(false);
+			nextButton5.setVisible(true);
+			step2.setVisible(true);
+			step2info.setVisible(true);
+		} else if (evt.getSource() == nextButton5) {
+			nextButton5.setVisible(false);
+			step2.setVisible(false);
+			step2info.setVisible(false);
+			helpPanel2.blnClickHit = true;
+			nextButton6.setVisible(true);
+			step2hit.setVisible(true);
+			step2hitinfo.setVisible(true);
+		} else if (evt.getSource() == nextButton6) {
+			helpPanel2.blnClickHit = false;
+			helpPanel2.blnClickMiss = true;
+			nextButton6.setVisible(false);
+			step2hit.setVisible(false);
+			step2hitinfo.setVisible(false);
+			step2miss.setVisible(true);
+			step2missinfo.setVisible(true);
+			nextButton7.setVisible(true);
+		} else if (evt.getSource() == nextButton7) {
+			// Reset everything
+			for (int intRow=1; intRow<=10; intRow++) {
+				for (int intCol=1; intCol<=10; intCol++) {
+					helpPanel2.intGrid[intRow][intCol] = 0;
+				}
+			}
+			helpPanel2.blnClickMiss = false;
+			step2miss.setVisible(false);
+			step2missinfo.setVisible(false);
+			nextButton7.setVisible(false);
+			helpPanel2.intPositionX = 800;
+			helpPanel2.intPositionY = 100;
+			helpPanel2.blnSelected = false;
+			helpPanel2.blnHorizontal = false;
+			helpPanel2.blnReady = false;
+			helpPanel2.blnClickHit = false;
+			helpPanel2.blnClickMiss = false;
+			step1.setVisible(true);
+			step1rotate.setVisible(true);
+			step1ready.setVisible(true);
+			nextButton2.setVisible(true);
+			step1yourMsgField.setText("");
+			step1opponentMsgField.setText("");
+			theFrame.setContentPane(homePanel);
+			theFrame.pack();
+			homePanel.repaint();
 		}
-		
-		
 		
 		if (evt.getSource() == theHelp) {
 			theFrame.setContentPane(helpPanel);
@@ -275,29 +389,74 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 	}
 	
 	public void mouseClicked(MouseEvent evt) {
-		int intRow = calcRow(evt.getY());
-		int intCol = calcRow(evt.getX());
-		System.out.println(intRow + " " + intCol);
-		
-		if (intRow >= 1 && intRow <= 10 && intCol >= 1 && intCol <= 10 && playPanel.blnYourTurn == true && playPanel.blnStartGame == true && playPanel.blnPlayingAnimation == false) {
-			if (playPanel.intOpponentGrid[intRow][intCol] != 0) {
-				return;
+		if (theFrame.getContentPane() == playPanel) {
+			int intRow = calcRow(evt.getY());
+			int intCol = calcRow(evt.getX());
+			System.out.println(intRow + " " + intCol);
+			
+			if (intRow >= 1 && intRow <= 10 && intCol >= 1 && intCol <= 10 && playPanel.blnYourTurn == true && playPanel.blnStartGame == true && playPanel.blnPlayingAnimation == false) {
+				if (playPanel.intOpponentGrid[intRow][intCol] != 0) {
+					return;
+				}
+				String strLetter = playPanel.strLetters[intCol];
+				String strNumber = Integer.toString(intRow);
+				
+				playPanel.blnPlayingAnimation = true;
+				playPanel.intAnimationRow = intRow;
+				playPanel.intAnimationCol = intCol;
+				playPanel.blnPlayAnimation[1] = true;
+				
+				yourMsgField.setText(strLetter + strNumber);
+				ssm.sendText(strLetter + strNumber);
 			}
-			String strLetter = playPanel.strLetters[intCol];
-			String strNumber = Integer.toString(intRow);
 			
-			playPanel.blnPlayingAnimation = true;
-			playPanel.intAnimationRow = intRow;
-			playPanel.intAnimationCol = intCol;
-			playPanel.blnPlayAnimation[1] = true;
+			if (playPanel.blnStartGame == true && playPanel.blnPlayingAnimation == false && playPanel.blnGameOver == true) {
+				if (evt.getX() > 830 && evt.getX() < 980 && evt.getY() > 177 && evt.getY() < 317) {
+					System.exit(1);
+				}
+			}
+		} else if (theFrame.getContentPane() == helpPanel2 && helpPanel2.blnClickHit == true) {
+			System.out.println("here");
+			int intRow = calcRow(evt.getY());
+			int intCol = calcRow(evt.getX());
 			
-			yourMsgField.setText(strLetter + strNumber);
-			ssm.sendText(strLetter + strNumber);
-		}
-		
-		if (playPanel.blnStartGame == true && playPanel.blnPlayingAnimation == false && playPanel.blnGameOver == true) {
-			if (evt.getX() > 830 && evt.getX() < 980 && evt.getY() > 177 && evt.getY() < 317) {
-				System.exit(1);
+			if (intRow >= 1 && intRow <= 10 && intCol >= 1 && intCol <= 10 && helpPanel2.blnPlayingAnimation == false) {
+				if (helpPanel2.intGrid[intRow][intCol] != 0) {
+					return;
+				}
+				helpPanel2.intGrid[intRow][intCol] = 1;
+				String strLetter = helpPanel2.strLetters[intCol];
+				String strNumber = Integer.toString(intRow);
+				
+				helpPanel2.blnPlayingAnimation = true;
+				helpPanel2.intAnimationRow = intRow;
+				helpPanel2.intAnimationCol = intCol;
+				helpPanel2.blnPlayAnimation[1] = true;
+				helpPanel2.blnPlayAnimation[2] = true;
+				
+				step1yourMsgField.setText(strLetter + strNumber);
+				step1opponentMsgField.setText("HIT");
+			}
+		} else if (theFrame.getContentPane() == helpPanel2 && helpPanel2.blnClickMiss == true) {
+			int intRow = calcRow(evt.getY());
+			int intCol = calcRow(evt.getX());
+			
+			if (intRow >= 1 && intRow <= 10 && intCol >= 1 && intCol <= 10 && helpPanel2.blnPlayingAnimation == false) {
+				if (helpPanel2.intGrid[intRow][intCol] != 0) {
+					return;
+				}
+				helpPanel2.intGrid[intRow][intCol] = 2;
+				String strLetter = helpPanel2.strLetters[intCol];
+				String strNumber = Integer.toString(intRow);
+				
+				helpPanel2.blnPlayingAnimation = true;
+				helpPanel2.intAnimationRow = intRow;
+				helpPanel2.intAnimationCol = intCol;
+				helpPanel2.blnPlayAnimation[1] = true;
+				helpPanel2.blnPlayAnimation[3] = true;
+				
+				step1yourMsgField.setText(strLetter + strNumber);
+				step1opponentMsgField.setText("MISS");
 			}
 		}
 	}
@@ -345,8 +504,9 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 			System.out.println("HERE");
 			int intX = evt.getX();
 			int intY = evt.getY();
-			if (helpPanel2.intPositionX == 800 && helpPanel2.intPositionY == 300 && intX >= 800 && intX <= 864 && intY >= 300 && intY <= 492) {
-				System.out.println("ADTY ");
+			if (helpPanel2.blnHorizontal == false && helpPanel2.intPositionX == 800 && helpPanel2.intPositionY == 100 && intX >= 800 && intX <= 864 && intY >= 100 && intY <= 292) {
+				helpPanel2.blnSelected = true;
+			} else if (helpPanel2.blnHorizontal == true && helpPanel2.intPositionX == 800 && helpPanel2.intPositionY == 100 && intX >= 800 && intX <= 992 && intY >= 100 && intY <= 164) {
 				helpPanel2.blnSelected = true;
 			}
 		}
@@ -386,9 +546,9 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 			int intRow = calcRow(evt.getY());
 			int intCol = calcCol(evt.getX());
 			
-			if (helpPanel2.blnSelected == true && (intRow < 1 || intRow > 10 || intCol < 1 || intCol > 10 || intRow + 3 - 1 > 10)) {
+			if (helpPanel2.blnSelected == true && (intRow < 1 || intRow > 10 || intCol < 1 || intCol > 10 || (helpPanel2.blnHorizontal == false && intRow + 3 - 1 > 10) || (helpPanel2.blnHorizontal == true && intCol + 3 - 1 > 10))) {
 				helpPanel2.intPositionX = 800;
-				helpPanel2.intPositionY = 300;
+				helpPanel2.intPositionY = 100;
 			} else if (helpPanel2.blnSelected == true) {
 				helpPanel2.intPositionX = (intCol - 1) * 64 + 80;
 				helpPanel2.intPositionY = (intRow - 1) * 64 + 80;
@@ -502,37 +662,120 @@ public class BattleshipGame implements ActionListener, MouseListener, MouseMotio
 		helpPanel2.setPreferredSize(new Dimension(1280, 720));
 		helpPanel2.addMouseListener(this);
 		helpPanel2.addMouseMotionListener(this);
-		step1.setBounds(300, 600, 600, 50);
+		step1.setBounds(300, 470, 600, 50);
 		step1.setFont(font1);
 		step1.setHorizontalAlignment(JTextField.CENTER);
 		step1.setEditable(false);
+		step1rotate.setBounds(230, 530, 700, 50);
+		step1rotate.setFont(font1);
+		step1rotate.setHorizontalAlignment(JTextField.CENTER);
+		step1rotate.setEditable(false);
+		step1ready.setBounds(280, 590, 600, 50);
+		step1ready.setFont(font1);
+		step1ready.setHorizontalAlignment(JTextField.CENTER);
+		step1ready.setEditable(false);
+		step1rotateButton.setFont(font1);
+		step1rotateButton.setBounds(1000, 360, 240, 80);
+		step1rotateButton.addActionListener(this);
+		step1readyButton.setFont(font1);
+		step1readyButton.setBounds(1000, 460, 240, 80);
+		step1readyButton.addActionListener(this);
+		step1yourMsgField.setFont(font1);
+		step1yourMsgField.setEditable(false);
+		step1yourMsgField.setBounds(1000, 560, 240, 60);
+		step1opponentMsgField.setFont(font1);
+		step1opponentMsgField.setEditable(false);
+		step1opponentMsgField.setBounds(1000, 640, 240, 60);
+		nextButton2.setBounds(540, 650, 200, 50);
+		nextButton2.setFont(font1);
+		nextButton2.addActionListener(this);
+		yourmsgfield.setBounds(400, 510, 500, 50);
+		yourmsgfield.setFont(font1);
+		yourmsgfield.setHorizontalAlignment(JTextField.CENTER);
+		yourmsgfield.setVisible(false);
+		yourmsgfield.setEditable(false);
+		opponentmsgfield.setBounds(300, 590, 660, 50);
+		opponentmsgfield.setFont(font1);
+		opponentmsgfield.setHorizontalAlignment(JTextField.CENTER);
+		opponentmsgfield.setVisible(false);
+		opponentmsgfield.setEditable(false);
+		bothready.setBounds(300, 590, 660, 50);
+		bothready.setFont(font1);
+		bothready.setHorizontalAlignment(JTextField.CENTER);
+		bothready.setVisible(false);
+		bothready.setEditable(false);
+		nextButton3.setBounds(540, 660, 200, 50);
+		nextButton3.setFont(font1);
+		nextButton3.addActionListener(this);
+		nextButton3.setVisible(false);
+		nextButton4.setVisible(false);
+		nextButton4.setBounds(540, 650, 200, 50);
+		nextButton4.setFont(font1);
+		nextButton4.addActionListener(this);
+		step2.setVisible(false);
+		step2.setBounds(10, 530, 1000, 50);
+		step2.setFont(font1);
+		step2.setHorizontalAlignment(JTextField.CENTER);
+		step2.setEditable(false);
+		step2info.setVisible(false);
+		step2info.setBounds(450, 590, 300, 50);
+		step2info.setFont(font1);
+		step2info.setHorizontalAlignment(JTextField.CENTER);
+		step2info.setEditable(false);
+		nextButton5.setVisible(false);
+		nextButton5.setBounds(540, 650, 200, 50);
+		nextButton5.setFont(font1);
+		nextButton5.addActionListener(this);
+		step2hit.setVisible(false);
+		step2hit.setBounds(180, 530, 800, 50);
+		step2hit.setFont(font1);
+		step2hit.setHorizontalAlignment(JTextField.CENTER);
+		step2hit.setEditable(false);
+		step2hitinfo.setVisible(false);
+		step2hitinfo.setBounds(200, 590, 770, 50);
+		step2hitinfo.setFont(font1);
+		step2hitinfo.setHorizontalAlignment(JTextField.CENTER);
+		step2hitinfo.setEditable(false);
+		nextButton6.setVisible(false);
+		nextButton6.setBounds(540, 650, 200, 50);
+		nextButton6.setFont(font1);
+		nextButton6.addActionListener(this);
+		step2miss.setVisible(false);
+		step2miss.setBounds(180, 530, 800, 50);
+		step2miss.setFont(font1);
+		step2miss.setHorizontalAlignment(JTextField.CENTER);
+		step2miss.setEditable(false);
+		step2missinfo.setVisible(false);
+		step2missinfo.setBounds(200, 590, 770, 50);
+		step2missinfo.setFont(font1);
+		step2missinfo.setHorizontalAlignment(JTextField.CENTER);
+		step2missinfo.setEditable(false);
+		nextButton7.setVisible(false);
+		nextButton7.setBounds(540, 650, 200, 50);
+		nextButton7.setFont(font1);
+		nextButton7.addActionListener(this);
 		helpPanel2.add(step1);
-		
-		/*help3Button.setBounds(1000, 600, 200, 80);
-		help3Button.setFont(font1);
-		help2Panel.add(help3Button);
-		help3Button.addActionListener(this);
-		
-		help3Panel.setLayout(null);
-		help3Panel.setPreferredSize(new Dimension(1280, 720));
-		help4Button.setBounds(1000, 600, 200, 80);
-		help4Button.setFont(font1);
-		help3Panel.add(help4Button);
-		help4Button.addActionListener(this);
-		
-		help4Panel.setLayout(null);
-		help4Panel.setPreferredSize(new Dimension(1280, 720));
-		help5Button.setBounds(1000, 600, 200, 80);
-		help5Button.setFont(font1);
-		help4Panel.add(help5Button);
-		help5Button.addActionListener(this);
-		
-		help5Panel.setLayout(null);
-		help5Panel.setPreferredSize(new Dimension(1280, 720));
-		help6Button.setBounds(1000, 600, 200, 80);
-		help6Button.setFont(font1);
-		help5Panel.add(help6Button);
-		help6Button.addActionListener(this);*/
+		helpPanel2.add(step1rotate);
+		helpPanel2.add(step1ready);
+		helpPanel2.add(step1rotateButton);
+		helpPanel2.add(step1readyButton);
+		helpPanel2.add(step1yourMsgField);
+		helpPanel2.add(step1opponentMsgField);
+		helpPanel2.add(nextButton2);
+		helpPanel2.add(yourmsgfield);
+		helpPanel2.add(opponentmsgfield);
+		helpPanel2.add(nextButton3);
+		helpPanel2.add(bothready);
+		helpPanel2.add(nextButton4);
+		helpPanel2.add(step2);
+		helpPanel2.add(step2info);
+		helpPanel2.add(nextButton5);
+		helpPanel2.add(step2hit);
+		helpPanel2.add(step2hitinfo);
+		helpPanel2.add(nextButton6);
+		helpPanel2.add(step2miss);
+		helpPanel2.add(step2missinfo);
+		helpPanel2.add(nextButton7);
 		
 		// Home Panel
 		homePanel.setLayout(null);
--- BattleshipHelpPanel2.java
+++ BattleshipHelpPanel2.java
@@ -10,15 +10,33 @@ public class BattleshipHelpPanel2 extends JPanel{
 	Font font1 = new Font("SansSerif", Font.BOLD, 20);
 	Font font2 = new Font("SansSerif", Font.BOLD, 50);
 	BufferedImage imgShip = null;
+	BufferedImage imgShipH = null;
+	BufferedImage imgShipMini = null;
+	BufferedImage imgShipMiniH = null;
 	BufferedImage imgPause = null;
 	BufferedImage imgLetters = null;
 	BufferedImage imgNumbers = null;
 	BufferedImage imgWater = null;
 	BufferedImage imgBox = null;
 	BufferedImage imgMinimap = null;
+	BufferedImage imgSprite = null;
+	BufferedImage imgBattleshipHit = null;
+	BufferedImage imgBattleshipMiss = null;
 	int intPositionX = 800;
-	int intPositionY = 300;
+	int intPositionY = 100;
 	boolean blnSelected = false;
+	boolean blnHorizontal = false;
+	boolean blnReady = false;
+	boolean blnClickHit = false;
+	boolean blnClickMiss = false;
+	boolean blnPlayAnimation[] = new boolean[4];
+	boolean blnPlayingAnimation = false;
+	int intMaxAnimationSprites[] = new int[4];
+	int intGrid[][] = new int[11][11];
+	int intAnimationRow = 0;
+	int intAnimationCol = 0;
+	int intAnimCount = 0;
+	String strLetters[] = new String[11];
 	
 	// Methods
 	public void paintComponent(Graphics g){
@@ -34,20 +52,86 @@ public class BattleshipHelpPanel2 extends JPanel{
 		g.drawImage(imgWater, 80, 80, null);
 		g.drawImage(imgBox, 720, 0, null);
 		g.drawImage(imgMinimap, 960, 0, null);
-		g.drawImage(imgShip, intPositionX, intPositionY, null);
+		if (blnHorizontal == false) {
+			if (blnReady == true) {
+				int intRow = (int)Math.floor(1.0 * (intPositionY - 80) / 64) + 1;
+				int intCol = (int)Math.floor(1.0 * (intPositionX - 80) / 64) + 1;
+				g.drawImage(imgShipMini, 960 + (intCol - 1) * 28 + 34, (intRow - 1) * 28 + 34, null);
+			} else {
+				g.drawImage(imgShip, intPositionX, intPositionY, null);
+			}
+		} else {
+			if (blnReady == true) {
+				int intRow = (int)Math.floor(1.0 * (intPositionY - 80) / 64) + 1;
+				int intCol = (int)Math.floor(1.0 * (intPositionX - 80) / 64) + 1;
+				g.drawImage(imgShipMiniH, 960 + (intCol - 1) * 28 + 34, (intRow - 1) * 28 + 34, null);
+			} else {
+				g.drawImage(imgShipH, intPositionX, intPositionY, null);
+			}
+		}
+		
+		for (int intRow=1; intRow<=10; intRow++) {
+			for (int intCol=1; intCol<=10; intCol++) {
+				if (intGrid[intRow][intCol] == 1) {
+					g.drawImage(imgBattleshipHit, (intCol - 1) * 64 + 80, (intRow - 1) * 64 + 80, null);
+				} else if (intGrid[intRow][intCol] == 2) {
+					g.drawImage(imgBattleshipMiss, (intCol - 1) * 64 + 80, (intRow - 1) * 64 + 80, null);
+				}
+			}
+		}
+		
+		if (blnPlayAnimation[1] == true) {
+			try {
+				imgSprite = ImageIO.read(new File("Assets/Sprites/BattleshipBomb"+Integer.toString(intAnimCount)+".png"));
+			} catch (IOException e) {
+				System.out.println("Error: bomb animation");
+			}
+			g.drawImage(imgSprite, (intAnimationCol - 1) * 64 + 80, (intAnimationRow - 1) * 64 + 80, null);
+		} else if (blnPlayAnimation[2] == true) {
+			try {
+				imgSprite = ImageIO.read(new File("Assets/Sprites/BattleshipHitExplosion"+Integer.toString(intAnimCount)+".png"));
+			} catch (IOException e) {
+				System.out.println("Error: bomb animation");
+			}
+			g.drawImage(imgSprite, (intAnimationCol - 1) * 64 + 80, (intAnimationRow - 1) * 64 + 80, null);
+		} else if (blnPlayAnimation[3] == true) {
+			try {
+				imgSprite = ImageIO.read(new File("Assets/Sprites/BattleshipSplash"+Integer.toString(intAnimCount)+".png"));
+			} catch (IOException e) {
+				System.out.println("Error: bomb animation");
+			}
+			g.drawImage(imgSprite, (intAnimationCol - 1) * 64 + 80, (intAnimationRow - 1) * 64 + 80, null);
+		}
 	}
 	// Constructor
 	public BattleshipHelpPanel2() {
         super();
-        
+        intMaxAnimationSprites[1] = 9;
+		intMaxAnimationSprites[2] = 5;
+		intMaxAnimationSprites[3] = 7;
+		strLetters[1] = "A";
+		strLetters[2] = "B";
+		strLetters[3] = "C";
+		strLetters[4] = "D";
+		strLetters[5] = "E";
+		strLetters[6] = "F";
+		strLetters[7] = "G";
+		strLetters[8] = "H";
+		strLetters[9] = "I";
+		strLetters[10] = "J";
         try {
+			imgBattleshipHit = ImageIO.read(new File("Assets/Sprites/BattleshipHit.png"));
+			imgBattleshipMiss = ImageIO.read(new File("Assets/Sprites/BattleshipMiss.png"));
 			imgShip = ImageIO.read(new File("Assets/Sprites/Battleship Theme/Battleship3TileSub.png"));
+			imgShipH = ImageIO.read(new File("Assets/Sprites/Battleship Theme/Battleship3TileSubH.png"));
 			imgPause = ImageIO.read(new File("Assets/Sprites/BattleshipPause.png"));
 			imgLetters = ImageIO.read(new File("Assets/Sprites/Battleship Theme/BattleshipLetters.png"));
 			imgNumbers = ImageIO.read(new File("Assets/Sprites/Battleship Theme/BattleshipNumbers.png"));
 			imgWater = ImageIO.read(new File("Assets/Sprites/BattleshipWater.png"));
 			imgBox = ImageIO.read(new File("Assets/Sprites/BattleshipBox.png"));
 			imgMinimap = ImageIO.read(new File("Assets/Sprites/BattleshipMinimap.png"));
+			imgShipMini = ImageIO.read(new File("Assets/Sprites/Battleship Theme/Battleship3TileSubMinimap.png"));
+			imgShipMiniH = ImageIO.read(new File("Assets/Sprites/Battleship Theme/Battleship3TileSubMinimapH.png"));
 		} catch (IOException e) {
 			System.out.println("Error: IMAGE");
 		}
